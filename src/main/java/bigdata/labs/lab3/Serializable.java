6a0c086a0f55167294d5c80d7d354d90448617e0
avonamardba <avonamardba@gmail.com>
2021-11-27 01:21:39 +0300

    Scripted auto-commit on change (2021-11-27 01:21:39) by gitwatch.sh

--- src/main/java/bigdata/labs/lab3/Serializable.java
+++ src/main/java/bigdata/labs/lab3/Serializable.java
@@ -1,17 +1,44 @@
 package bigdata.labs.lab3;
 
-public class Serializable {
-    private int originAirportID;
-    private int destAirportID;
-    private String delayTime;
-    private String cancelled;
+import java.io.Serializable;
 
-    public Serializable(int originAirportID, int destAirportID, String delayTime, String cancelled) {
-        this.originAirportID = originAirportID;
-        this.destAirportID = destAirportID;
-        this.delayTime = delayTime;
-        this.cancelled = cancelled;
+public class flightData implements Serializable {
+    private int delayedFlights;
+    private int cancelledFlights;
+    private float maxDelayTime;
+    private int flights;
+
+    public flightData(int delayedFlights, int cancelledFlights, int maxDelayTime, int
+            flights) {
+        this.delayedFlights = delayedFlights;
+        this.cancelledFlights = cancelledFlights;
+        this.maxDelayTime = maxDelayTime;
+        this.flights = flights;
     }
 
-    p
+    public int getDelayedFlights() {
+        return delayedFlights;
+    }
+
+    public int getCancelledFlights() {
+        return cancelledFlights;
+    }
+
+    public float getMaxDelayTime() {
+        return maxDelayTime;
+    }
+
+    public int getFlights() {
+        return flights;
+    }
+
+    private float getDelayedPercentage() {
+        return (float) delayedFlights / flights * 100;
+    }
+
+    private float getCancelledPercentage() {
+        return (float) cancelledFlights / flights * 100;
+    }
+
+    
 }
