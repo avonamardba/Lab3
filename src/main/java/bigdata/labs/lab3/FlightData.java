78b93f25d04cbc982bb7e322aa9f6f0e57af6fd8
avonamardba <avonamardba@gmail.com>
2021-11-27 01:34:53 +0300

    Scripted auto-commit on change (2021-11-27 01:34:53) by gitwatch.sh

--- src/main/java/bigdata/labs/lab3/FlightData.java
+++ src/main/java/bigdata/labs/lab3/FlightData.java
@@ -2,18 +2,25 @@ package bigdata.labs.lab3;
 
 import java.io.Serializable;
 
-public class flightData implements Serializable {
-    private int delayedFlights;
-    private int cancelledFlights;
-    private float maxDelayTime;
-    private int flights;
+public class FlightData implements Serializable {
+    private final int delayedFlights;
+    private final int cancelledFlights;
+    private final float maxDelayTime;
+    private final int flights;
 
-    public flightData(int delayedFlights, int cancelledFlights, int maxDelayTime, int
+    public FlightData(int delayedFlights, int cancelledFlights, int maxDelayTime, int
             flights) {
         this.delayedFlights = delayedFlights;
         this.cancelledFlights = cancelledFlights;
         this.maxDelayTime = maxDelayTime;
         this.flights = flights;
+    }
+
+    public FlightData(String d, String c) {
+        this.delayedFlights = d.equals("") ? 0 : 1;
+        this.cancelledFlights = Integer.parseInt(c) > 0 ? 1 : 0;
+        this.maxDelayTime = d.equals("") ? 0 : Float.parseFloat(d);
+        this.flights = 1;
     }
 
     public int getDelayedFlights() {
@@ -39,6 +46,4 @@ public class flightData implements Serializable {
     private float getCancelledPercentage() {
         return (float) cancelledFlights / flights * 100;
     }
-
-    
 }
