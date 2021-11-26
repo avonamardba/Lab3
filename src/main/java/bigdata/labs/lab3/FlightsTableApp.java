1b3ab0a6aa026bedc5b3696c08772e689519832a
avonamardba <avonamardba@gmail.com>
2021-11-26 23:59:09 +0300

    Scripted auto-commit on change (2021-11-26 23:59:09) by gitwatch.sh

--- src/main/java/bigdata/labs/lab3/FlightsTableApp.java
+++ src/main/java/bigdata/labs/lab3/FlightsTableApp.java
@@ -5,12 +5,16 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightsTableApp {
-    private static String 
+    private static String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
+    private static String AIRPORTS_FILE = "L_AIRPORT_ID.csv";
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> flightsFile = sc.textFile()
+        JavaRDD<String> flightsLines = sc.textFile(FLIGHTS_FILE).;
+        JavaRDD<String> airportsLines = sc.textFile(AIRPORTS_FILE);
+
 
     }
 }
