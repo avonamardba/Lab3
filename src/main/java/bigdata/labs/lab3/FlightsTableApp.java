169a037eec6b5467d3a0a2620d0fbb415e3cfb78
avonamardba <avonamardba@gmail.com>
2021-11-26 23:44:22 +0300

    Scripted auto-commit on change (2021-11-26 23:44:22) by gitwatch.sh

--- src/main/java/bigdata/labs/lab3/FlightsTableApp.java
+++ src/main/java/bigdata/labs/lab3/FlightsTableApp.java
@@ -1,5 +1,12 @@
 package bigdata.labs.lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class FlightsTableApp {
-    public static void main
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        
+    }
 }
