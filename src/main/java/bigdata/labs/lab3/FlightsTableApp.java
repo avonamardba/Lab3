35cec47929ee63ba365915dd0bc0f993670a1cf8
avonamardba <avonamardba@gmail.com>
2021-11-27 09:44:23 +0300

    Scripted auto-commit on change (2021-11-27 09:44:23) by gitwatch.sh

--- src/main/java/bigdata/labs/lab3/FlightsTableApp.java
+++ src/main/java/bigdata/labs/lab3/FlightsTableApp.java
@@ -23,32 +23,32 @@ public class FlightsTableApp {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-//        JavaRDD<String> flightsLines = sc.textFile(FLIGHTS_FILE);
-//        JavaRDD<String> airportsLines = sc.textFile(AIRPORTS_FILE);
-//
-//        JavaRDD<String[]> flParsed = flightsLines
-//                .map(Parser::getColumns)
-//                .filter(columns -> !columns[DEST_AIRPORT_ID].equals(DEST_AIRPORT_COLUMN_NAME));
-//
-//        JavaRDD<String[]> alParsed = airportsLines
-//                .map(Parser::getColumns)
-//                .filter(columns -> !columns[AIRPORT_ID].equals(AIRPORT_COLUMN_NAME));
-//
-//        JavaPairRDD<Tuple2, FlightData> flightPairs = flParsed
-//                .mapToPair(columns -> {
-//                    FlightsParser p = new FlightsParser(columns);
-//                    return new Tuple2<>(new Tuple2<>(p.getOriginAirportID(), p.getDestAirportID()),
-//                            new FlightData(p.getDelay(), p.getCancelled()));
-//                });
-//
-//        JavaPairRDD<Tuple2, FlightData> flightsTable = flightPairs.reduceByKey(FlightData::reduceMethod);
-//
-//        final Broadcast<Map<Integer, String>> airportsBroadcast = sc.broadcast(alParsed
-//                .mapToPair(columns -> {
-//                    FlightsParser p = new FlightsParser(columns);
-//                    return new Tuple2<>(p.getAirportID(), p.getAirportName());
-//                }).collectAsMap());
-//        JavaRDD<String> output = flightsTable.map(p -> String.format("%s, %s, %s", airportsBroadcast.value().get(p._1._1), airportsBroadcast.value().get(p._1._2), p._2.getResult()));
-//        output.saveAsTextFile("output");
+        JavaRDD<String> flightsLines = sc.textFile(FLIGHTS_FILE);
+        JavaRDD<String> airportsLines = sc.textFile(AIRPORTS_FILE);
+
+        JavaRDD<String[]> flParsed = flightsLines
+                .map(Parser::getColumns)
+                .filter(columns -> !columns[DEST_AIRPORT_ID].equals(DEST_AIRPORT_COLUMN_NAME));
+
+        JavaRDD<String[]> alParsed = airportsLines
+                .map(Parser::getColumns)
+                .filter(columns -> !columns[AIRPORT_ID].equals(AIRPORT_COLUMN_NAME));
+
+        JavaPairRDD<Tuple2, FlightData> flightPairs = flParsed
+                .mapToPair(columns -> {
+                    FlightsParser p = new FlightsParser(columns);
+                    return new Tuple2<>(new Tuple2<>(p.getOriginAirportID(), p.getDestAirportID()),
+                            new FlightData(p.getDelay(), p.getCancelled()));
+                });
+
+        JavaPairRDD<Tuple2, FlightData> flightsTable = flightPairs.reduceByKey(FlightData::reduceMethod);
+
+        final Broadcast<Map<Integer, String>> airportsBroadcast = sc.broadcast(alParsed
+                .mapToPair(columns -> {
+                    FlightsParser p = new FlightsParser(columns);
+                    return new Tuple2<>(p.getAirportID(), p.getAirportName());
+                }).collectAsMap());
+        JavaRDD<String> output = flightsTable.map(p -> String.format("%s, %s, %s", airportsBroadcast.value().get(p._1._1), airportsBroadcast.value().get(p._1._2), p._2.getResult()));
+        output.saveAsTextFile("output");
     }
 }
